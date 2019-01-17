e1b9aa8bdf6d596a79c172403d2088d46e62bf31
Sho Iwamoto <contact@misho-web.com>
2019-01-17 18:07:15 +0100

    v0.1.4

--- heprefs/heprefs.py
+++ heprefs/heprefs.py
@@ -13,7 +13,7 @@ from .cds_article import CDSArticle
 from .inspire_article import InspireArticle
 
 __author__ = 'Sho Iwamoto / Misho'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __license__ = 'MIT'
 
 basicConfig(level=DEBUG)
