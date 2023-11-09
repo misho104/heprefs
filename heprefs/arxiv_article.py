65bc9f69b0a391fe5ebdaa0f79fcd3af1b566544
Sho Iwamoto <iwamoto@g-mail.nsysu.edu.tw>
2023-11-09 20:54:53 +0800

    bump dependencies (arxiv)

--- heprefs/arxiv_article.py
+++ heprefs/arxiv_article.py
@@ -3,46 +3,33 @@ import re
 import os
 import arxiv
 import feedparser
+import urllib
 from logging import getLogger
 
 logger = getLogger(__name__)
 
 
 class ArxivArticle(object):
-    SERVER = "https://arxiv.org"
-    API = "http://export.arxiv.org/api/query"
-
     OLD_FORMAT_DEFAULT = "hep-ph"
 
     @classmethod
     def get_info(cls, arxiv_id):
-        # Code from arxiv.py https://github.com/lukasschwab/arxiv.py
-        query_url = "{}?id_list={}".format(cls.API, arxiv_id)
-        results = feedparser.parse(query_url)
-        if results.get("status") != 200:
-            raise Exception(
-                "Failed to fetch arXiv article information: HTTP Error "
-                + str(results.get("status", "no status"))
-            )
-
-        if (
-            len(results["entries"]) == 0 or "id" not in results["entries"][0]
-        ):  # because arXiv api returns one blank entry even if nothing found
+        client = arxiv.Client()
+        search = arxiv.Search(id_list=[arxiv_id])
+        result = list(arxiv.Client().results(search))
+        if len(result) == 0:
             raise Exception("arXiv:{} not found".format(arxiv_id))
-        elif len(results["entries"]) > 1:
+        elif len(result) > 1:
             warning_text = (
                 "more than one entries are found, whose titles are" + os.linesep
             )
-            for i in results:
+            for i in result:
                 title = i.get("title", dict()).get("title") or "unknown " + i.get(
                     "primary_report_number"
                 )
                 warning_text += "    " + title + os.linesep
             logger.warning(warning_text)
-
-        result = results["entries"][0]
-        arxiv.mod_query_result(result)
-        return result
+        return result[0]
 
     @classmethod
     def shorten_author(cls, author):
@@ -64,6 +51,7 @@ class ArxivArticle(object):
         self._arxiv_id = None
         self.arxiv_id = arxiv_id
         self._info = None
+        self._server_name = ""
 
     @property
     def arxiv_id(self):
@@ -96,26 +84,31 @@ class ArxivArticle(object):
             self._info = self.get_info(self.arxiv_id)
         return self._info
 
+    def _url(self, key):
+        if not self._server_name:
+            self._server_name = urllib.parse.urlparse(self.info.entry_id).netloc
+        return f"https://{self._server_name}/{key}/{self.arxiv_id}"
+
     def abs_url(self):
-        return "{}/abs/{}".format(self.SERVER, self.arxiv_id)
+        return self._url("abs")
 
     def pdf_url(self):
-        return "{}/pdf/{}".format(self.SERVER, self.arxiv_id)
+        return self._url("pdf")
 
     def source_url(self):
-        return "{}/e-print/{}".format(self.SERVER, self.arxiv_id)
+        return self._url("e-print")
 
     def title(self):
-        return re.sub(r"\s+", " ", self.info["title"])
+        return re.sub(r"\s+", " ", self.info.title)
 
     def authors(self):
-        return ", ".join(self.info["authors"])
+        return ", ".join(str(a) for a in self.info.authors)
 
     def first_author(self):
-        return self.info["authors"][0]
+        return self.info.authors[0]
 
     def authors_short(self):
-        authors = [self.shorten_author(a) for a in self.info["authors"]]
+        authors = [self.shorten_author(str(a)) for a in self.info.authors]
         if len(authors) > 5:
             authors = authors[0:4] + ["et al."]
         return ", ".join(authors)
@@ -123,7 +116,7 @@ class ArxivArticle(object):
     def download_parameters(self):
         authors = self.authors_short().replace(", ", "-").replace("et al.", "etal")
         filename = "{id}-{authors}.pdf".format(id=self.arxiv_id, authors=authors)
-        return self.info["pdf_url"], filename
+        return self.info.pdf_url, filename
 
     def debug(self):
         data = {
